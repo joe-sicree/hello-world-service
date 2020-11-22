b0617dc5602a3658b45774a8c28aeabacd99a5ce
Joe Sicree <jsicree@gmail.com>
2020-11-21 22:09:30 -0500

    Initial Commit

---
+++ src/test/java/com/joesicree/helloworld/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joesicree.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
